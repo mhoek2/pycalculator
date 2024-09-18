13ab80ff9a3e56eef56d357cf1bb32c7f2b2e013
Skyrlie <joycepolling@gmail.com>
2024-09-18 11:49:48 +0200

    Update koppelcode.py

--- koppelcode.py
+++ koppelcode.py
@@ -1,11 +1,13 @@
 import random
 from turtledemo.chaos import jumpto
 
-# tuple instead of list because tuple allows you to access individual elements by using index and can't be changed.
 alphabet = ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
 
-random_code2 = ""
-for i in range(0, 4):
-    random_letter = alphabet[random.randint(0,24)]
-    random_code2 += random_letter
-print(random_code2)
+def get_random_code(num_letters):
+    random_code2 = ""
+    for i in range(0, num_letters):
+        random_letter = alphabet[random.randint(0,24)]
+        random_code2 += random_letter
+    return random_code2
+random_code = get_random_code(4)
+print(random_code)
