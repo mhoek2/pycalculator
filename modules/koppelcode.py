3bebc3e3c0344adcb4bc2922e37a4a9ae78ac794
Skyrlie <joycepolling@gmail.com>
2024-09-18 12:03:30 +0200

    add random code function

--- modules/koppelcode.py
+++ modules/koppelcode.py
@@ -1,3 +1,6 @@
+import random
+from turtledemo.chaos import jumpto
+
 class KoppelCode:
     """Contains methods used to create a random uppercase string of letters"""
     def __init__( self, context ) -> None:
@@ -5,6 +8,9 @@ class KoppelCode:
         self.m_settings = context.m_settings
         self.m_renderer = context.m_renderer
         self.m_gui = context.m_gui
+        self.alphabet = (
+        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "V", "W",
+        "X", "Y", "Z")
 
         print("KoppelCode init")
 
@@ -15,4 +21,13 @@ class KoppelCode:
     # self.m_context.m_koppelcode.functionName()
 
     def update( self ) -> None:
+        random_code = self.get_random_code(4)
+        print(random_code)
         return
+
+    def get_random_code(self, num_letters):
+        random_code2 = ""
+        for i in range(0, num_letters):
+            random_letter = self.alphabet[random.randint(0,24)]
+            random_code2 += random_letter
+        return random_code2
