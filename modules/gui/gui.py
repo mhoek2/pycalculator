c5defc2907f73ef1bb12e849c8634e96f3ec6ea7
Marco H <153363103+mhoek2@users.noreply.github.com>
2024-09-18 09:22:43 +0200

    Add examples on how to call methods from other modules

--- modules/gui/gui.py
+++ modules/gui/gui.py
@@ -7,6 +7,12 @@ class Gui:
 
 		print("Gui init")
 
+    # Example on how to call functions from other modules:
+    # self.m_context.m_gui.functionName()
+    # self.m_context.m_filestore.functionName()
+    # self.m_context.m_calculator.functionName()
+    # self.m_context.m_koppelcode.functionName()
+
 	def update( self ) -> None:
 		return
 
